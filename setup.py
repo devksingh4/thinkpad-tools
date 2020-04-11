5132b3bc9a9d38b40d4395dcc37c93158a6dda8b
Dev Singh <dev@singhk.dev>
2020-04-11 11:10:48 -0500

    fix linting errors

--- setup.py
+++ setup.py
@@ -12,8 +12,8 @@ import sys
 setup(
     name='thinkpad-tools',
     maintainer="Dev Singh",
-    maintainer_email="devksingh4@gmail.com",
-    version='0.9.3',
+    maintainer_email="dev@singhk.dev",
+    version='0.10.0',
     description='Tools for ThinkPads',
     platforms=['Linux'],
     keywords='thinkpad trackpoint battery undervolt',
@@ -23,5 +23,6 @@ setup(
 )
 
 print("Will now install the systemd unit service for persistence.")
-print("To set persistent settings, please edit the file '/etc/thinkpad-tools-persistence.sh'")
+print("""To set persistent settings, please edit the file
+      '/etc/thinkpad-tools-persistence.sh'""")
 os.system('sudo python3 persistence.py')
